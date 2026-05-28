64217af55c0b89223fb37190573695ec89beb6c5
amircs2004 <saidimahdiamir@gmail.com>
2026-05-28 20:38:45 +0100

    deleted that process.exit(1)

--- connection/connectDB.js
+++ connection/connectDB.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
      console.log('Connected to MongoDB successfully');
     }catch (error) {
         console.error('Error connecting to MongoDB:', error); 
-        process.exit(1);
+    throw error;
     }
 }
 module.exports = connectDB
