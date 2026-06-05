42707db1e6249ad6bdcbbdb5edf77d690ad38288
amircs2004 <saidimahdiamir@gmail.com>
2026-06-05 21:10:33 +0100

    fix: remove invalid options wildcard and refresh paths

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
 app.use(express.json())
 app.use(cookieParser());
 // Routes
