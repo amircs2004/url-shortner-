dea6a2ecb6b0ba0a19309e9cb891391b3171bebd
amircs2004 <saidimahdiamir@gmail.com>
2026-06-06 22:28:17 +0100

    fix: force lowercase product model resolution for linux

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product'); 
+const Product = require('../models/product');
 
 const addNewProduct = async (req, res) => {
   const { name, description, price, category, stock, barcode, imageUrl } = req.body;
